87c5e5f4cc960280484f6d2008a2f06da6aecbec
Ammar Yasser <105017866+AmmarYasser33@users.noreply.github.com>
2022-08-31 20:58:06 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Code/s18/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "ef8e188c-a44a-47f0-8aec-acf5ee05b416";
+export const MODAL_CLOSE_SEC = 2.5;
