d729c126d34b190db3cd7945a7ea1505cd2245f0
Ammar Yasser <105017866+AmmarYasser33@users.noreply.github.com>
2022-08-31 21:08:40 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/Data Structures, Modern Operators and Strings/s9-challenge3.js
@@ -0,0 +1,30 @@
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// Answer
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+for (const [key, value] of gameEvents) {
+  const half = key >= 45 ? "SECOND HALF" : "FIRST HALF";
+  console.log(`[${half}] ${key}: ${value}`);
+}
